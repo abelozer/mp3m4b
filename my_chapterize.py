146ceb646b0922361a8a376a3f0fb776356dbf8d
Alexey Belozerov <abelozer@cisco.com>
2023-09-19 13:59:04 +0200

    fully restructured code

--- my_chapterize.py
+++ my_chapterize.py
@@ -9,10 +9,24 @@ import sys
 import argparse
 import subprocess
 import json
+import logging
 from pathlib import Path
 from tqdm import tqdm
 from mutagen.mp3 import MP3
+from settings import *
 
+file_log_format = "%(asctime)s::%(levelname)s::%(name)s::"\
+             "%(filename)s::%(lineno)d::%(message)s"
+logging.basicConfig(filename='history.log', level='DEBUG', format=file_log_format)
+console_log_format = '%(message)s'
+logger = logging.getLogger(__name__)
+logger.setLevel('DEBUG')
+
+# Use FileHandler() to log to a file
+file_handler = logging.StreamHandler()
+formatter = logging.Formatter(console_log_format)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 def get_length_using_mutagen(file_path: Path) -> float:
     """
@@ -38,7 +52,16 @@ def get_length_using_ffprobe(file_path: Path) -> float:
     Returns:
         float: The length of the audio file in nanoseconds.
     """
-    result = subprocess.check_output(["ffprobe", file_path, "-show_entries", "format=duration", "-v", "quiet", "-of", "csv=p=0"])
+    result = subprocess.check_output([
+        "ffprobe",
+        file_path,
+        "-show_entries",
+        "format=duration",
+        "-v",
+        "quiet",
+        "-of",
+        "csv=p=0"
+    ])
     ffprobe_length = float(result) * 1e9 * 1.0005
     #1.0001
     return ffprobe_length
@@ -62,191 +85,248 @@ def get_chapter_title(file_path: Path, tag: str) -> str:
         "-print_format",
         "json",
         "-show_format"
-    ]    
+    ]
     try:
         output = subprocess.check_output(ffprobe_command)
         chapter_text_metadata = output.decode('utf-8')
         json_metadata = json.loads(chapter_text_metadata)
-        chapter_title = json_metadata["format"]["tags"][tag]
+        format_tags = json_metadata.get("format", {}).get("tags", {})
+        chapter_title = format_tags.get(tag, "")
         return chapter_title
-    except (subprocess.CalledProcessError, KeyError, json.JSONDecodeError) as e:
-        print(f"Error: {e}")
+    except subprocess.CalledProcessError as error:
+        logger.error("ffprobe command failed with error: %s", error)
+        return ""
+    except json.JSONDecodeError as error:
+        logger.error("Error decoding JSON metadata: %s", error)
+        return ""
+    except KeyError as error:
+        logger.error("Tag %s not found in metadata: %s", tag, error)
         return ""
 
-def print_metadata(starttimes: list) -> None:
+def print_metadata(title_length_list: list) -> None:
     """
     Helper function
     """
-    for item in starttimes:
+    for item in title_length_list:
         print(" - ".join(item))
 
-def extract_artwork_from_mp3(artwork_filename: str) -> bool:
+def extract_artwork_from_mp3(artwork_filename: str, folder_path: Path, input_files: list) -> bool:
     """
     Extracts artwork from source mp3 file.
     Takes the first file in the list.
     Returns True if successfull.
     """
-    print(f'Trying to extract artwork from the source mp3 file...')
-    shell_command = f"ffmpeg -i '{input_audio_files[0]}' -an -vcodec copy {artwork_filename}"
+    logger.info('Trying to extract artwork from the source mp3 file...')
+    shell_command = f"ffmpeg -i '{input_files[0]}' -an -vcodec copy {artwork_filename}"
     os.system(shell_command)
     file_path = os.path.join(folder_path, artwork_filename)
     if os.path.exists(file_path):
         return True
     else:
-        print("Unable to extract artwork.")
-        return False
-
-def check_file(artwork_filename: str) -> bool:
-    """
-    Checks if the artwork file exists.
-    If it does not exist it tries to get artwork from source mp3 files.
-    Args:
-        artwork_filename(str): filename to look for
-    Returns:
-        Boolean
-    """
-    if os.path.exists(artwork_filename):
-        return True
-    else:
+        logger.warning("Unable to extract artwork.")
         return False
 
 def attach_artwork(temp_file: str, artwork_filename: str, output_file_name: str) -> None:
     """
     Attaches artwork to the newly created audiobook file.
     """
-    os.system(f"AtomicParsley '{temp_file}' --artwork {artwork_filename} -o '{output_file_name}.m4b'")
+    shell_command = f"AtomicParsley '{temp_file}' --artwork {artwork_filename} -o '{output_file_name}.m4b'"
+    os.system(shell_command)
     os.remove(temp_file)
 
-parser = argparse.ArgumentParser(description='Concatenates audio files and adds chapter markers.')
+def parse_agruments():
+    """
+    Parses arguments and assignes default values if no arguments provided
+    Returns:
+        folder_path
+        bitrate
+        book_title
+        artwork_filename
+        output_file_name
+    """
+    parser = argparse.ArgumentParser(description='Concatenates audio files and adds chapter markers.')
 
-# Add arguments to the parser
-parser.add_argument('-p', '--path', type=str, help='Path to folder with mp3 files')
-parser.add_argument('-b', '--bitrate', type=str, help='Bitrate of audiobook  (96k, 128k etc)')
-parser.add_argument('-t', '--title', type=str, help='Audiobook title')
-parser.add_argument('-a', '--artwork', type=str, help='Audiobook cover image')
-parser.add_argument('-o', '--output', type=str, help='Output file name without extension')
+    # Add arguments to the parser
+    parser.add_argument('-p',
+                        '--path',
+                        type=str,
+                        help='Path to folder with mp3 files',
+                        default=Path('./'))
+    # parser.add_argument('-b',
+    #                     '--bitrate',
+    #                     type=str,
+    #                     help='Bitrate of audiobook  (96k, 128k etc)',
+    #                     default=None)
+    parser.add_argument('-t',
+                        '--title',
+                        type=str,
+                        help='Audiobook title (tag)',
+                        default=None)
+    parser.add_argument('-a',
+                        '--artwork',
+                        type=str,
+                        help='Audiobook cover image filename',
+                        default=default_artwork_filename)
+    parser.add_argument('-o',
+                        '--output',
+                        type=str,
+                        help='Output file name without extension',
+                        default="audiobook")
 
-# Parse the arguments
-args = parser.parse_args()
+    # Parse the arguments
+    args = parser.parse_args()
 
-if args.path:
-    folder_path = Path(args.path)
-    print(f"Path: {folder_path}")
-else:
-    folder_path = Path('./')
-    print("Path: trying current folder")
+    # Folder
+    if os.path.exists(args.path):
+        folder_path = Path(args.path)
+        logger.info("Path: %s", folder_path)
+    else:
+        logger.critical("Path %s does not exist. Exiting", args.path)
+        sys.exit()
 
-# Checking bitrate
-if args.bitrate:
-    bitrate = args.bitrate
-    print(f'Bitrate: {args.bitrate}')
-else:
-    bitrate = ""
-    print('Default bitrate: we\'ll try to get it from mp3 files')
+    # # Bitrate
+    # bitrate = args.bitrate
+    # logger.info(f'Bitrate: {args.bitrate}')
 
-# Checking title
-if args.title:
+    # Audiobook title (tag)
     book_title = args.title
-    print(f'Title: {args.title}')
-else:
-    book_title = ""
-    print('Default title: we\'ll try to get it from mp3 tag Album')
+    logger.info('Audiobook title: %s', args.title)
 
-# Checking artwork
-if args.artwork:
-    # Check if specified file exists
-    if check_file(args.artwork):
+    # Checking artwork
+    if os.path.exists(args.artwork):
         artwork_filename = args.artwork
-        print(f'Artwork: {args.artwork}')
+        logger.info('Artwork: %s', args.artwork)
     else:
-        print(f'Specified artwork file {args.artwork} does not exist.')
-else:
-    artwork_filename = "cover.jpg"
-    # Check if default artwork file 'cover.jpg exist
-    if check_file(artwork_filename):
-        print(f'Default Artwork file {artwork_filename} is found.')
-    else:
-        # Try to extract file from mp3
-        if(not extract_artwork_from_mp3(artwork_filename)):
-            artwork_filename = None
+        logger.warning('Artwork: file not found')
+        artwork_filename = None
 
-# Checking output file name
-if args.output:
+    # Checking output file name
     output_file_name = args.output
-    print(f'Output file name: {args.output}.m4b')
-else:
-    output_file_name = "audiobook"
-    print(f'Default output file name: {output_file_name}.m4b')
+    logger.info('Output file name: %s.m4b', args.output)
 
-# Getting files from source folder
-os.chdir(str(folder_path))
-input_audio_files = sorted(str(file) for file in folder_path.glob('*.mp3'))
+    # abook = {
+    #     "folder_path": folder_path,
+    #     "book_title": book_title,
+    #     "artwork_filename": artwork_filename,
+    #     "output_file_name": output_file_name
+    # }
+    abook_vars = folder_path, book_title, artwork_filename, output_file_name
+    return abook_vars
 
-if len(input_audio_files) == 0:
-    print(f"{os.getcwd()} does not contain mp3 files")
-    sys.exit()
-genre = "Audiobook"
-comment = ""
-chapter_title_tag = "title"
-ffmetadata_file = "FFMETADATA.txt"
+def get_file_list(folder_path: Path) -> list:
+    """
+    Get a list of sourse mp3 files
+    Args:
+        folder_path: folder to search files
+    Returns:
+        input_files: a sorded list of mp3 files
+    """
+    # Getting files from source folder
+    os.chdir(str(folder_path))
+    input_files = sorted(str(file) for file in folder_path.glob('*.mp3'))
 
+    if len(input_files) == 0:
+        logger.critical("%s does not contain mp3 files", os.getcwd())
+        sys.exit()
+    return input_files
 
-starttimes=[]
-ffprobtime = 0.0 #cummulative start time (nanoseconds)
-for audio_file in tqdm(input_audio_files, desc='Processing mp3 files'):
-    chapter_title = get_chapter_title(Path(audio_file), chapter_title_tag)
-    # ffprobtime += get_length_using_ffprobe(Path(audio_file))
-    ffprobtime += get_length_using_mutagen(Path(audio_file))
-    starttimes.append([audio_file, chapter_title, str(int(ffprobtime))])
+def build_title_length_list(input_files: list) -> list:
+    """
+    Args:
+        mp3 file list
+    Returns:
+        title_length_list: list of lists [filename, chapter_title, chapter_length]
+    """
+    # Building list with chapters name and length
+    title_length_list=[]
+    ffprobtime = 0.0 #cummulative start time (nanoseconds)
 
-output = subprocess.run(["ffprobe", input_audio_files[0], "-v", "quiet", "-print_format", "json", "-show_format"], stdout=subprocess.PIPE, universal_newlines=True)
-textMetadata = output.stdout
-data = json.loads(textMetadata)
-if bitrate == "":
+    for audio_file in tqdm(input_files, desc='Processing mp3 files'):
+        chapter_title = get_chapter_title(Path(audio_file), chapter_title_tag)
+        if chapter_title == "":
+            logger.warning("Unable to get chapter title from %s file", audio_file)
+        # ffprobtime += get_length_using_ffprobe(Path(audio_file))
+        ffprobtime += get_length_using_mutagen(Path(audio_file))
+        title_length_list.append([audio_file, chapter_title, str(int(ffprobtime))])
+
+    return title_length_list
+
+def main():
+    """
+    Main function
+    """
+
+    #logger.basicConfig(filename='history.log', encoding='utf-8', level=logger.DEBUG)
+
+    folder_path, book_title, artwork_filename, output_file_name = parse_agruments()
+    input_files = get_file_list(folder_path)
+    title_length_list = build_title_length_list(input_files)
+
+    # Getting mp3 file metadata to extract bitrate and book_title
+    output = subprocess.run([
+        "ffprobe",
+        input_files[0],
+        "-v",
+        "quiet",
+        "-print_format",
+        "json",
+        "-show_format"],
+        stdout=subprocess.PIPE,
+        universal_newlines=True,
+        check=False
+        )
+    text_metadata = output.stdout
+    data = json.loads(text_metadata)
     bitrate = data["format"]["bit_rate"][:-3] + "k"
-tags = data["format"]["tags"]
-if book_title == "":
-    if 'album' in tags:
-        tags['title']=tags['album']
+    tags = data["format"]["tags"]
+    if book_title is None:
+        if 'album' in tags:
+            tags['title']=tags['album']
+        else:
+            tags['title'] = input("Enter audiobook title tag: ")
+    tags['genre']=genre
+
+    # https://ffmpeg.org/ffmpeg-formats.html#Metadata-1
+    # "If the timebase is missing then start/end times are assumed to be in 𝗻𝗮𝗻𝗼𝘀𝗲𝗰𝗼𝗻𝗱𝘀."
+    # "chapter start and end times in form ‘START=num’, ‘END=num’, where num is a 𝗽𝗼𝘀𝗶𝘁𝗶𝘃𝗲 𝗶𝗻𝘁𝗲𝗴𝗲𝗿."
+
+    with open(ffmetadata_file, 'w', encoding="utf-8") as metafile:
+        print(";FFMETADATA1", file=metafile)
+        print("title=" + tags['title'], file=metafile)
+        print("comment=" + comment, file=metafile)
+        if 'artist' in tags:
+            print("artist=" + tags['artist'], file=metafile)
+        if 'album_artist' in tags:
+            print("album_artist=" +tags['album_artist'], file=metafile)
+        print("genre=" + tags['genre'], file=metafile)
+        if 'date' in tags:
+            print("date=" + tags['date'], file=metafile)
+        if 'publisher' in tags:
+            print("publisher=" + tags['publisher'], file=metafile)
+        start = "0"
+        for i in title_length_list:
+            print("", file=metafile)
+            print("[CHAPTER]", file=metafile)
+            print("START=" + start, file=metafile)
+            print("END=" + i[2], file=metafile)
+            print("title=" + i[1], file=metafile)
+            start = str(int(i[2])+1)
+
+    bar_separated_filenames = ''
+    for i in input_files:
+        bar_separated_filenames += i+'|'
+
+    temp_file = output_file_name + '.m4a'
+    shell_command = 'ffmpeg -i "concat:' + bar_separated_filenames[:-1]  + '" -i ' + ffmetadata_file + ' -map_metadata 1 -vn -b:a ' + bitrate + ' \'' + temp_file + '\''
+    os.system(shell_command)
+
+    if artwork_filename:
+        attach_artwork(temp_file, artwork_filename, output_file_name)
     else:
-        tags['title'] = "Audiobook"
-tags['genre']=genre
+        if extract_artwork_from_mp3(default_artwork_filename, folder_path, input_files):
+            attach_artwork(temp_file, artwork_filename, output_file_name)
 
-# https://ffmpeg.org/ffmpeg-formats.html#Metadata-1
-# "If the timebase is missing then start/end times are assumed to be in 𝗻𝗮𝗻𝗼𝘀𝗲𝗰𝗼𝗻𝗱𝘀."
-# "chapter start and end times in form ‘START=num’, ‘END=num’, where num is a 𝗽𝗼𝘀𝗶𝘁𝗶𝘃𝗲 𝗶𝗻𝘁𝗲𝗴𝗲𝗿."
+    print('\a')
 
-with open(ffmetadata_file, 'w') as metafile:
-    print(";FFMETADATA1", file=metafile)
-    print("title=" + tags['title'], file=metafile)
-    print("comment=" + comment, file=metafile)
-    if 'artist' in tags:
-        print("artist=" + tags['artist'], file=metafile)
-    if 'album_artist' in tags:
-        print("album_artist=" +tags['album_artist'], file=metafile)
-    print("genre=" + tags['genre'], file=metafile)
-    if 'date' in tags:
-        print("date=" + tags['date'], file=metafile)
-    if 'publisher' in tags:
-        print("publisher=" + tags['publisher'], file=metafile)
-    start = "0"
-    for i in starttimes:
-        print("", file=metafile)
-        print("[CHAPTER]", file=metafile)
-        print("START=" + start, file=metafile)
-        print("END=" + i[2], file=metafile)
-        print("title=" + i[1], file=metafile)
-        start = str(int(i[2])+1)
-
-bar_separated_filenames = ''
-for i in input_audio_files:
-    bar_separated_filenames += i+'|'
-
-temp_file = output_file_name + '.m4a'
-os.system('ffmpeg -i "concat:' + bar_separated_filenames[:-1]  + '" -i ' + ffmetadata_file + ' -map_metadata 1 -vn -b:a ' + bitrate + ' \'' + temp_file + '\'')
-
-if artwork_filename:
-    attach_artwork(temp_file, artwork_filename, output_file_name)
-
-print('\a')
-print('Done!')
+if __name__ == "__main__":
+    main()
---
+++ settings.py
@@ -0,0 +1,5 @@
+genre = "Audiobook"
+comment = ""
+chapter_title_tag = "title"
+ffmetadata_file = "FFMETADATA.txt"
+default_artwork_filename = "cover.jpg"
